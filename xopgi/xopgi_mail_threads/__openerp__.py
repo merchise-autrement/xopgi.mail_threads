2c293bc7e6c591442c437337de41f912738a285f
Manuel Vázquez Acosta <mva.led@gmail.com>
2014-08-25 16:35:37 -0400

    Jump to version 1.1.

--- xopgi/xopgi_mail_threads/__openerp__.py
+++ xopgi/xopgi_mail_threads/__openerp__.py
@@ -14,7 +14,7 @@
 
 {
     "name": "Mail Threads (xopgi)",
-    "version": "1.0.1",
+    "version": "1.1",
     "author": "Merchise Autrement",
     "website": "http://xopgi.merchise.org/addons/xopgi_mail_threads",
     'category': 'Social Network',
