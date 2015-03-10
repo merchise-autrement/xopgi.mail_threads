4d286c11610cb2f1aa1787ebc1aa15f52a5aabe1
Manuel Vázquez Acosta <mva.led@gmail.com>
2015-03-10 12:42:19 -0400

    Chain transport by default.

--- xopgi/xopgi_mail_threads/transports.py
+++ xopgi/xopgi_mail_threads/transports.py
@@ -105,7 +105,7 @@ class MailTransportRouter(metaclass(RegisteredType)):
         '''
         raise NotImplemented()
 
-    def deliver(self, obj, cr, uid, message, data, context=None):
+    def deliver(self, server, cr, uid, message, data, context=None):
         '''Deliver if possible the message.
 
         Return False if the transport won't do the delivery directly.  This
@@ -129,7 +129,8 @@ class MailTransportRouter(metaclass(RegisteredType)):
         not defined.
 
         '''
-        return False
+        kwargs = dict(data or {}, context=context)
+        return server.send_email(cr, uid, message, **kwargs)
 
     def prepare_message(self, obj, cr, uid, message, context=None):
         '''Prepares the message to be delivered.
