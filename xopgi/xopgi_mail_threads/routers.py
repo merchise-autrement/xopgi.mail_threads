535cdae1e2df1d80db6ffc4b778a80c5a838ee4a
Manuel Vázquez Acosta <manu@merchise.org>
2015-06-10 18:58:03 -0400

    Future-proof xoutil.

--- xopgi/xopgi_mail_threads/routers.py
+++ xopgi/xopgi_mail_threads/routers.py
@@ -17,7 +17,10 @@ from __future__ import (division as _py3_division,
                         absolute_import as _py3_abs_import)
 
 import warnings
-from xoutil.objects import metaclass
+try:
+    from xoutil.eight.meta import metaclass
+except ImportError:
+    from xoutil.objects import metaclass
 
 from .utils import RegisteredType
 
