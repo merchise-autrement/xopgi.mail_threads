1bc9ec61b824fef1f635695e162916fa0cd77846
Manuel Vázquez Acosta <manu@merchise.org>
2015-06-12 10:57:03 -0400

    Add utils for replacing messages address-related headers.

--- xopgi/xopgi_mail_threads/utils.py
+++ xopgi/xopgi_mail_threads/utils.py
@@ -17,6 +17,13 @@ from __future__ import (division as _py3_division,
                         absolute_import as _py3_abs_import)
 
 
+from email.utils import getaddresses, formataddr
+
+from openerp.addons.mail.mail_thread import decode_header
+from openerp.addons.base.ir.ir_mail_server import \
+    encode_rfc2822_address_header as _address_header
+
+
 class RegisteredType(type):
     '''A metaclass that registers all its instances.'''
 
@@ -43,3 +50,54 @@ def is_router_installed(cr, uid, router):
             return bool(mm.search(cr, uid, query))
     else:
         return False
+
+
+def set_message_address_header(message, header, value, address_only=False):
+    if address_only:
+        previous = decode_header(message, header)
+        previous = getaddresses([previous])
+        names = [name for name, _ in previous]
+    else:
+        names = []
+    replacements = getaddresses([value])
+    if names:
+        from six.moves import zip
+        from itertools import cycle
+        addresses = list(
+            zip(names, cycle(email for _, email in replacements))
+        )
+    else:
+        addresses = replacements
+    value = _address_header(
+        ', '.join(formataddr(address) for address in addresses)
+    )
+    if header in message:
+        message.replace_header(header, value)
+    else:
+        message[header] = value
+
+
+def set_message_sender(message, sender, address_only=False):
+    '''Set or replace the message's sender header.
+
+    If `sender` contains both the name and address both are changed unless
+    `address_only` is True, in which case the name of the sender in the
+    message survives.
+
+    '''
+    set_message_address_header(message, 'Sender', sender,
+                               address_only=address_only)
+
+
+def set_message_from(message, addresses, address_only=False):
+    '''Set or replace the message's From header.
+
+    :type addresses: string
+
+    :param addresses: The addresses Can contain several email addresses (with
+                      or without names).  It can be a single address and all
+                      authors will be replaced.
+
+    '''
+    set_message_address_header(message, 'From', addresses,
+                               address_only=address_only)
