4d33e8225a78decb3c82848b0547ed6e4d8c9e8d
Manuel Vázquez Acosta <manuel@merchise.org>
2019-02-07 15:57:05 -0500

    Remove outdate __openerp__ files.

--- xopgi/xopgi_mail_threads/__manifest__.py
+++ xopgi/xopgi_mail_threads/__manifest__.py
@@ -1,1 +1,28 @@
-__openerp__.py
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# Copyright (c) Merchise Autrement [~º/~] and Contributors
+# All rights reserved.
+#
+# This is free software; you can do what the LICENCE file allows you to.
+#
+
+{
+    "name": "Mail Threads (xopgi)",
+    "version": "4.1",
+    "post_load": "post_load_hook",
+    "author": "Merchise Autrement",
+    "website": "http://xopgi.merchise.org/addons/xopgi_mail_threads",
+    'category': 'Social Network',
+    "description": "Improves OpenERP's basic mail management.",
+    "depends": ['mail'],
+    "data": [
+        "views/transitional.xml"
+    ],
+    "application": False,
+    "auto_install": True,
+
+    # MIGRATION POLICY: All addons are not included until someone work on them
+    # and upgrade them.
+    'installable': 8 <= MAJOR_ODOO_VERSION < 12,  # noqa
+}
