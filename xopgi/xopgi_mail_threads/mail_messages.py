381274a5ff07788b27160dfc616cf306405def77
Manuel Vázquez Acosta <manuel@merchise.org>
2019-05-09 18:40:30 -0400

    Fix setting values of raw_email.
    Binary should take with a base64 encoded string.  We no longer need to do
    Reencoding to the target connection encoding.

--- xopgi/xopgi_mail_threads/mail_messages.py
+++ xopgi/xopgi_mail_threads/mail_messages.py
@@ -24,13 +24,17 @@ from __future__ import (division as _py3_division,
 import logging
 
 from xoutil.eight.string import force as force_str
-from xoutil.future.codecs import safe_encode, safe_decode
 
 from xoeuf import fields, api, models
 
 from email import message_from_string
 from email.generator import DecodedGenerator
 from email.message import Message
+
+try:
+    from base64 import encodebytes
+except ImportError:
+    from base64 import encodestring as encodebytes
 
 
 #: The name of the field to store the raw email.
@@ -52,8 +56,7 @@ class MailMessage(models.Model):
 # that, in turn, call `message_parse` this is the place to make the raw_email
 # stuff happen, not the `mail_message` object.
 class MailThread(models.AbstractModel):
-    _name = 'mail.thread'
-    _inherit = _name
+    _inherit = 'mail.thread'
 
     @api.model
     def message_parse(self, message, save_original=False):
@@ -66,13 +69,9 @@ class MailThread(models.AbstractModel):
         try:
             from io import BytesIO
             buf = BytesIO()
-            # Re-encode to the connection encoding
-            gen = ReencodingGenerator(buf, mangle_from_=False,
-                                      target_charset=self._cr._cnx.encoding)
+            gen = DecodedGenerator(buf, mangle_from_=False)
             gen.flatten(message)
-            message = safe_decode(buf.getvalue(),
-                                  encoding=self._cr._cnx.encoding)
-            result[RAW_EMAIL_ATTR] = message
+            result[RAW_EMAIL_ATTR] = encodebytes(buf.getvalue())
         except Exception:  # noqa
             # Should any error happen while reencoding; it's not worthy to
             # stop the message from being created.  Just log.
@@ -80,43 +79,3 @@ class MailThread(models.AbstractModel):
                 'Error while re-encoding raw email.  Continuing normally'
             )
         return result
-
-
-# TODO: When we are in Python 3, this won't be needed: we'll be able to use
-# DecodedGenerator directly.
-class ReencodingGenerator(DecodedGenerator):
-    '''A generator that re-encodes text bodies to a given charset.
-
-    A message with 'text/...' Content-Type will be re-encoded (if needed) to
-    the target charset.
-
-    '''
-    def __init__(self, outfp, mangle_from_=True, maxheaderlen=78,
-                 target_charset='utf-8'):
-        from email.charset import Charset, QP
-        self._target_charset = target_charset
-        self.charset = charset = Charset(target_charset)
-        charset.body_encoding = QP
-        DecodedGenerator.__init__(
-            self,
-            outfp,
-            mangle_from_=mangle_from_,
-            maxheaderlen=maxheaderlen,
-        )
-
-    def clone(self, fp):
-        result = DecodedGenerator.clone(self, fp)
-        result._target_charset = self._target_charset
-        return result
-
-    def write(self, s):
-        DecodedGenerator.write(self, safe_encode(s, self._target_charset))
-
-    # Internal API for Python 3.
-
-    def _new_buffer(self):
-        from io import BytesIO
-        return BytesIO()
-
-    def _encode(self, s):
-        return safe_encode(s, self._target_charset)
